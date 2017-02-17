04f72cb36e54f3ec2ef85b7dec5c2237c64fbfdc
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-02-17 16:37:26 +0100

    Fixed mergeObject - check for null or undefined

--- src/polyfills.js
+++ src/polyfills.js
@@ -6,11 +6,13 @@ function mergeObject(target) {
     m = arguments.length;
     for (i = 1; i < m; i += 1) {
         source = arguments[i];
-        keys = Object.keys(source);
-        n = keys.length;
-        for (j = 0; j < n; j += 1) {
-            key = keys[j];
-            target[key] = source[key];
+        if ('object' === typeof source) {
+            keys = Object.keys(source);
+            n = keys.length;
+            for (j = 0; j < n; j += 1) {
+                key = keys[j];
+                target[key] = source[key];
+            }
         }
     }
     return target;
