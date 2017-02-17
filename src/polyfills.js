c273c9206426f8bf32a2ddf175369c9e226fb3d0
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-02-17 16:31:53 +0100

    Added Object.assign polyfill

---
+++ src/polyfills.js
@@ -0,0 +1,19 @@
+
+// Alternative for Object.assign
+function mergeObject(target) {
+    'use strict';
+    var i, j, m, n, source, key, keys;
+    m = arguments.length;
+    for (i = 1; i < m; i += 1) {
+        source = arguments[i];
+        keys = Object.keys(source);
+        n = keys.length;
+        for (j = 0; j < n; j += 1) {
+            key = keys[j];
+            target[key] = source[key];
+        }
+    }
+    return target;
+}
+
+exports.mergeObject = mergeObject;
