aa213d1f39a68df3c89f36f88dcace14844ad1b0
Dmitry Myadzelets <dmitrymyadzelets@gmail.comgit config --global user.email dmitrymyadzelets@gmail.com>
2017-02-19 19:20:46 +0100

    mobile debug

--- main.js
+++ main.js
@@ -1818,7 +1818,7 @@ function init() {
         im('messages: ' + data.messages.length
                 + '<br>' + chat.config.data.messages.length
                 + '<br>' + chat.config.data.nested.length
-                + '<br>' + [][-1]
+                + '<br>' + Number.MAX_SAFE_INTEGER
                 + '<br> :-)');
     }());
 
